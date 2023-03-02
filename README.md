7c0340da40d74883c8b7e21af113ee20e3c5d996
ShirleyofHuang <shirley.huang2000@outlook.com>
2023-03-02 10:57:40 -0500

    adding cloudwatch logging'

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Setup
 1. npm install
-2. npm start
+2. npm start for production environment OR npm dev for development environment
--- src/index.js
+++ src/index.js
@@ -13,7 +13,35 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
+// Setup cloudwatch for production logs. If in dev env then logs will 
+// continue to go to console. Production will go to cloudwatch
+const winston = require('winston'),
+      WinstonCloudWatch = require('winston-cloudwatch');
+
+const logger = new winston.createLogger({
+    format: winston.format.json(),
+    transports: [
+        new (winston.transports.Console)({
+            timestamp: true,
+            colorize: true,
+        })
+   ]
+});
+if (process.env.NODE_ENV === 'production') {
+const cloudwatchConfig = {
+    logGroupName: process.env.CLOUDWATCH_GROUP_NAME,
+    logStreamName: process.env.NODE_ENV,
+    awsAccessKeyId: process.env.CLOUDWATCH_ACCESS_KEY,
+    awsSecretKey: process.env.CLOUDWATCH_SECRET_ACCESS_KEY,
+    awsRegion: process.env.CLOUDWATCH_REGION,
+    messageFormatter: ({ level, message, additionalInfo }) =>    `[${level}] : ${message} \nAdditional Info: ${JSON.stringify(additionalInfo)}}`
+}
+    logger.add(new WinstonCloudWatch(cloudwatchConfig))
+}
+
 // Application start
 app.listen(port, () => {
     console.log(`Klear-backend listening on port ${port}`);
 });
+
+module.exports = logger;
