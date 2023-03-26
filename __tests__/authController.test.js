cfbd45dfd1675a5453df28bcc9d3fb28121e69ae
ShirleyofHuang <shirley.huang2000@outlook.com>
2023-03-26 10:55:14 -0400

    Cleaning up

--- __tests__/authController.test.js
+++ __tests__/authController.test.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcrypt")
 const dbConnection = require("../src/config/dbConnection");
 const app = require('../src/app');
 const jwt = require("jsonwebtoken")
-const generateAccessTokenModule = require("../src/config/generateAccessToken");
 
 function delay() {
     return new Promise((resolve, reject) => {
@@ -21,20 +20,22 @@ describe('Integration tests for register', () => {
     let sandbox = sinon.createSandbox();
     let mysqlMock
     let bcryptMock;
-    let accessTokenMock;
-    beforeEach(() => {
+    const user = {
+        firstName: "Jane",
+        lastName: "Doe",
+        email: "blah@blah.com",
+        password: "some_password_hash"
+    };
 
+    beforeEach(() => {
         bcryptMock = sandbox.stub(bcrypt, "hash");
         bcryptMock.resolves("some_random_hash")
 
-        // accessTokenMock = sandbox.stub(jwt, "sign");
-        // accessTokenMock.returns("random")
         stub = sinon.stub(jwt, 'sign').callsFake(() => {
             return "random";
         });
 
         mysqlMock = sinon.mock(dbConnection);
-
     });
 
     afterEach(() => {
@@ -44,33 +45,14 @@ describe('Integration tests for register', () => {
     });
 
     it("should not insert a new user since it exists", async function () {
-
-        // var mysqlMock = sinon.mock(dbConnection);
-        const user = {
-            firstName: "Jane",
-            lastName: "Doe",
-            email: "blah@blah.com",
-            password: "some_password_hash"
-        };
         var results = [user];
         var expectation = mysqlMock.expects('query')
                 .callsArgWith(1, null, results);
-
-        //Preform
         const response = await request(app).post('/register').send(user)
         expect(response.statusCode).toBe(409)
-
-        sinon.restore()
     });
 
     it ("should register a new user", async function () {
-        const user = {
-            firstName: "Jane",
-            lastName: "Doe",
-            email: "blah@blah.com",
-            password: "some_password_hash"
-        };
-        
         results = []
         mysqlMock.expects('query')
             .atLeast(1)
@@ -84,7 +66,6 @@ describe('Integration tests for register', () => {
         const response = await request(app).post('/register').send(user)
         expect(response.statusCode).toBe(200)
         expect(response.body.accessToken).toBe('random')
-        sinon.restore()
     })
 
 })
@@ -93,16 +74,17 @@ describe('Integration tests for login', () => {
     let sandbox = sinon.createSandbox();
     let mysqlMock
     let bcryptMock;
+    const user = {
+        email: "login@login.com", 
+        password: "123"
+    }
     beforeEach(() => {
         stub = sinon.stub(jwt, 'sign').callsFake(() => {
             return "random";
         });
-        // accessTokenMock = sandbox.stub(generateAccessTokenModule, "generateAccessToken");
-        // accessTokenMock.returns("random")
 
         mysqlMock = sinon.mock(dbConnection);
         bcryptMock = sandbox.stub(bcrypt, "compare");
-
     });
 
     afterEach(() => {
@@ -112,10 +94,6 @@ describe('Integration tests for login', () => {
     })
 
     it('should fail to login is no user', async function () {
-        const user = {
-            email: "login@login.com", 
-            password: "pw"
-        }
         results = []
         var expectation = mysqlMock.expects('query')
             .callsArgWith(1, null, results);
@@ -123,33 +101,22 @@ describe('Integration tests for login', () => {
         expect(response.statusCode).toBe(401)
     });
 
-    it('should fail to login if password is incorrect', async function () {
-       
+    it('should fail to login if password is incorrect', async function () { 
         bcryptMock.resolves(false);
-        const user = {
-            email: "login@login.com", 
-            password: "123"
-        }
         results = [user]
         var expectation = mysqlMock.expects('query')
             .callsArgWith(1, null, results);
         const response = await request(app).post('/login').send(user)
         expect(response.text).toBe("Password incorrect!")
-        bcryptMock.reset()
     });
 
     it('should login the user with correct email and password', async function () {
         bcryptMock.resolves(true);
-        const user = {
-            email: "login@login.com", 
-            password: "123"
-        }
         results = [user]
         var expectation = mysqlMock.expects('query')
             .callsArgWith(1, null, results);
         const response = await request(app).post('/login').send(user)
         expect(response.statusCode).toBe(200)
         expect(response.body.accessToken).toBe("random")
-        bcryptMock.restore()
     })
 })
