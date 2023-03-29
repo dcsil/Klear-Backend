bf2a391043273687f46019ca922b606ddfdd4f13
MERCUTAIME <ahou2000@hotmail.com>
2023-03-29 13:52:02 -0400

    Add api that fetches student page info

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config()
 
 const sampleRoute = require("./routes/sampleRoute");
+const studentRoute = require("./routes/studentRoute")
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use(bodyParser.json());
 
 // Route endpoints
 app.use('/sampleRouteMiddleName/', sampleRoute);
+app.use('/student/', studentRoute);
 
 module.exports = app
---
+++ src/routes/studentRoute.js
@@ -0,0 +1,54 @@
+const express = require("express");
+var router = express.Router();
+const Sentry = require("@sentry/node");
+const dbConnection = require('../config/dbConnection');
+
+/* Get all students */
+router.get("/all", function (req, res) { 
+    dbConnection.query("SELECT * FROM students",
+    (err, results, fields) => {
+      if (!err) {
+        res.send(results);
+      } else {
+        Sentry.captureException(new Error("Something went wrong :/"));
+      }
+    });
+})
+
+/* Get activities under the students */
+router.get("/activities", function (req, res) { 
+    dbConnection.query("SELECT * FROM activities WHERE student_id = " + req.body.student_id,
+    (err, results, fields) => {
+      if (!err) {
+        res.send(results);
+      } else {
+        Sentry.captureException(new Error("Something went wrong :/"));
+      }
+    });
+})
+
+/* Get student info */
+router.get("/info", function (req, res) { 
+    dbConnection.query("SELECT * FROM students WHERE student_id = " + req.body.student_id,
+    (err, results, fields) => {
+      if (!err) {
+        res.send(results);
+      } else {
+        Sentry.captureException(new Error("Something went wrong :/"));
+      }
+    });
+})
+
+/* Get student incidents */
+router.get("/incidents", function (req, res) { 
+    dbConnection.query("SELECT * FROM `student_incidents` JOIN incidents ON student_incidents.incident_id = incidents.incident_id WHERE student_id = " + req.body.student_id,
+    (err, results, fields) => {
+      if (!err) {
+        res.send(results);
+      } else {
+        Sentry.captureException(new Error("Something went wrong :/"));
+      }
+    });
+})
+
+module.exports = router
