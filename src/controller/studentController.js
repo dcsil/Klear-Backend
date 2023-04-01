ecda6ef1987c1c5cc009770bf37fe8b16e55aa91
MERCUTAIME <ahou2000@hotmail.com>
2023-04-01 01:20:42 -0400

    Update indentation, sort students

--- src/controller/studentController.js
+++ src/controller/studentController.js
@@ -6,72 +6,72 @@ const verifyAccessToken = require('../config/jwtHelper').verifyAccessToken
 
 /* Health check */
 exports.health = (req,res) => {
-    return res.json({ hello: 'Hello from the backend!' })
+  return res.json({ hello: 'Hello from the backend!' })
 };
 
 /* Get all students */
 exports.all = (req, res) => { 
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (!verifyAccessToken(token)) return res.sendStatus(401)
-    dbConnection.query("SELECT * FROM students",
-    (err, results, fields) => {
-      if (!err) {
-        res.send(results);
-      } else {
-        Sentry.captureException(new Error("When getting all students: "+err));
-      }
-    });
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (!verifyAccessToken(token)) return res.sendStatus(401)
+  dbConnection.query("SELECT * FROM students ORDER BY first_name",
+  (err, results, fields) => {
+    if (!err) {
+      res.send(results);
+    } else {
+      Sentry.captureException(new Error("When getting all students: "+err));
+    }
+  });
 };
 
 /* Get student info */
 exports.info = (req, res) => { 
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (!verifyAccessToken(token)) return res.sendStatus(401)
-    if (!req.body.hasOwnProperty("student_id")) return res.sendStatus(400)
-    if (req.body.student_id == null) return res.sendStatus(400)
-    dbConnection.query("SELECT * FROM students WHERE student_id = " + req.body.student_id,
-    (err, results, fields) => {
-      if (!err) {
-        res.send(results);
-      } else {
-        Sentry.captureException(new Error("When getting student info: "+err));
-      }
-    });
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (!verifyAccessToken(token)) return res.sendStatus(401)
+  if (!req.body.hasOwnProperty("student_id")) return res.sendStatus(400)
+  if (req.body.student_id == null) return res.sendStatus(400)
+  dbConnection.query("SELECT * FROM students WHERE student_id = " + req.body.student_id,
+  (err, results, fields) => {
+    if (!err) {
+      res.send(results);
+    } else {
+      Sentry.captureException(new Error("When getting student info: "+err));
+    }
+  });
 };
 
 /* Get student incidents and activities */
 exports.history = (req, res) => { 
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (!verifyAccessToken(token)) return res.sendStatus(401)
-    if (!req.body.hasOwnProperty("student_id")) return res.sendStatus(400)
-    if (req.body.student_id == null) return res.sendStatus(400)
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (!verifyAccessToken(token)) return res.sendStatus(401)
+  if (!req.body.hasOwnProperty("student_id")) return res.sendStatus(400)
+  if (req.body.student_id == null) return res.sendStatus(400)
 
-    const results = [];
-    dbConnection.query("SELECT * FROM `student_incidents` JOIN incidents ON student_incidents.incident_id = incidents.incident_id WHERE student_id = " + req.body.student_id,
-    (err, incidents, fields) => {
-      if (!err) {
-        for (var i = 0; i < incidents.length; i++) {
-            incidents[i]["type"] = "incident";
-            results.push(incidents[i]);
-        }
-      } else {
-        Sentry.captureException(new Error("When getting student incidents: "+ err));
+  const results = [];
+  dbConnection.query("SELECT * FROM `student_incidents` JOIN incidents ON student_incidents.incident_id = incidents.incident_id WHERE student_id = " + req.body.student_id,
+  (err, incidents, fields) => {
+    if (!err) {
+      for (var i = 0; i < incidents.length; i++) {
+          incidents[i]["type"] = "incident";
+          results.push(incidents[i]);
       }
-    });
-    dbConnection.query("SELECT * FROM activities WHERE student_id = " + req.body.student_id,
-    (err, activities, fields) => {
-      if (!err) {
-        for (var i = 0; i < activities.length; i++) {
-            activities[i]["type"] = "activity";
-            results.push(activities[i]);
-        }
-        res.send(results);
-      } else {
-        Sentry.captureException(new Error("When getting student activities: "+ err));
+    } else {
+      Sentry.captureException(new Error("When getting student incidents: "+ err));
+    }
+  });
+  dbConnection.query("SELECT * FROM activities WHERE student_id = " + req.body.student_id,
+  (err, activities, fields) => {
+    if (!err) {
+      for (var i = 0; i < activities.length; i++) {
+        activities[i]["type"] = "activity";
+        results.push(activities[i]);
       }
-    });
+      res.send(results);
+    } else {
+      Sentry.captureException(new Error("When getting student activities: "+ err));
+    }
+  });
     
 };
