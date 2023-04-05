c59250558bf4f2f5c8676dbc39106cfa8c332e34
natyho <natalie200078@gmail.com>
2023-04-05 19:39:13 -0400

    Update human_activity_reco.py

--- cvservice/human_activity_reco.py
+++ cvservice/human_activity_reco.py
@@ -18,8 +18,8 @@ import os
 import time
 
 if __name__ == '__main__':
-    # input_file = 'kids_running.mp4'
-    input_file = 0
+    input_file = 'kids_crying.mp4'
+    # input_file = 0
 
     args = {"classes": "action_recognition_kinetics.txt", "model": "resnet-34_kinetics.onnx", "input": input_file}
 
@@ -58,34 +58,39 @@ if __name__ == '__main__':
         outputs = net.forward()
         outputs_sorted = np.sort(outputs, axis=None)
         print(outputs_sorted[-1], outputs_sorted[-2],)
-        # if outputs_sorted[-1] >= 1.3* outputs_sorted[-2]:
-        label = CLASSES[np.argmax(outputs)]
-        print(label)
-        if label == 'crying' or label == 'doing aerobics' or label == 'zumba':
-            if label == 'doing aerobics' or label == 'zumba':
-                label = 'indoor sprint'
+        if outputs_sorted[-1] >= 1.3* outputs_sorted[-2]:
+            label = CLASSES[np.argmax(outputs)]
+            print(label)
+            if label == 'crying' or label == 'doing aerobics' or label == 'zumba':
+                if label == 'doing aerobics' or label == 'zumba':
+                    label = 'indoor sprint'
 
-            filename = '../upload' + label + str(datetime.now()) + '.jpg'
+                os.chdir('../upload')
+                filename =  label + str(datetime.now()) + '.jpg'
 
-            print(cv2.imwrite(filename, frame))
-            url = 'http://3.142.235.3:3000/incidents/add'
-            incident = {'event': label, 'date': str(datetime.now()), 'imageUrl': filename[9:] }
-            x = requests.post(url, json=incident)
-            print(x.text)
+                print(cv2.imwrite(filename, frame))
+                url = 'http://3.142.235.3:3000/incidents/add'
+                if label == 'indoor sprint':
+                    image_url = 'indoor_sprint.jpg'
+                if label == 'crying':
+                    image_url = 'crying.jpg'
+                incident = {'event': label, 'date': str(datetime.now()), 'imageUrl': image_url}
+                x = requests.post(url, json=incident)
+                print(x.text)
 
-            header = {"Content-Type": "application/json; charset=utf-8",
-                      "Authorization": "Basic NTUzZTY3MWUtNDRkMC00MTg2LWFjZmItMDIwZTlkMmNiMWFm"}
+                header = {"Content-Type": "application/json; charset=utf-8",
+                          "Authorization": "Basic NTUzZTY3MWUtNDRkMC00MTg2LWFjZmItMDIwZTlkMmNiMWFm"}
 
-            payload = {"app_id": "9ea06b9a-1433-4c15-ba13-3e8c70a63f52",
-                       "included_segments": ["Subscribed Android Users"],
-                       "contents": {"en": "Incident Detected: " + label}}
+                payload = {"app_id": "9ea06b9a-1433-4c15-ba13-3e8c70a63f52",
+                           "included_segments": ["Subscribed Android Users"],
+                           "contents": {"en": "Incident Detected: " + label}}
 
-            req = requests.post("https://onesignal.com/api/v1/notifications", headers=header,
-                                data=json.dumps(payload))
-            print("notification", req)
-            time.sleep(180)
-        # else:
-        #     label = ''
+                req = requests.post("https://onesignal.com/api/v1/notifications", headers=header,
+                                    data=json.dumps(payload))
+                print("notification", req)
+                time.sleep(180)
+        else:
+            label = ''
 
         cv2.rectangle(frame, (0, 0), (300, 40), (0, 0, 0), -1)
         cv2.putText(frame, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
